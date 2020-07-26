404499757b5a20bec777c5f8ab91b44bb6e7a667
Daniel Liu <hnnk@qq.com>
2020-07-26 12:14:05 +0800

    parcel sass swiper worked

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 import anime from 'animejs/lib/anime.es.js';
+
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.button-next',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
